ddbfacbfb3b01711f3ed23d6122a1f10a83327e9
예슬_노트북\tulip <tuliphan91@gmail.com>
2022-06-30 15:43:17 +0900

    fix checkbox boolean으로 수정

--- static/js/article_create.js
+++ static/js/article_create.js
@@ -1,7 +1,12 @@
 function handelArticleCreate(){
-    
+
     const title = document.getElementById("title").value
-    const is_active = document.getElementById("is_active").value
+    let is_active = document.getElementById("is_active").value
+    if(is_active=="on"){
+        is_active=true
+    }else{
+        is_active=false
+    }
     const exposure_end_date = document.getElementById("exposure_end_date").value
     postArticle(title,is_active, exposure_end_date)
 }
