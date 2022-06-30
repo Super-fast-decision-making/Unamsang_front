fadac068b2f3fb02eb6bf1aaa2e2fcbe0e0728c6
예슬_노트북\tulip <tuliphan91@gmail.com>
2022-06-30 14:58:01 +0900

    edit 정리

--- static/js/api.js
+++ static/js/api.js
@@ -17,9 +17,18 @@ async function postArticle(title,is_active, exposure_end_date){
     console.log(response_json)
     
     if (response.status==200){
-        window.location.replace(`${frontend_base_url}/`);
+        alert(response.status);//http://127.0.0.1:5500/main.html
     }else{
         alert(response.status)
     }
 
+}
+
+async function getArticles(){
+    const response = await fetch('http://127.0.0.1:8000/article',{
+        method:'GET',
+    })
+    response_json = await response.json()
+    console.log(response._json)
+    return response_json.articles
 }
--- static/js/article_create.js
+++ static/js/article_create.js
@@ -1,10 +1,20 @@
 function handelArticleCreate(){
+    
     const title = document.getElementById("title").value
     const is_active = document.getElementById("is_active").value
     const exposure_end_date = document.getElementById("exposure_end_date").value
-    // console.log(title)
-    // console.log(is_active)
-    // console.log(exposure_end_date)
-
     postArticle(title,is_active, exposure_end_date)
+}
+
+async function loadArticles(){
+    console.log("here")
+    articles= await getArticles()
+
+    const article_list= document.getElementById("articles")
+    articles.forEach(element => {
+        const newArticle = document.createElement("li")
+        newArticle.setAttribute("id", article.id)
+        newArticle.innerText = article.titlearticle_list.append(newArticle)
+        
+    });
 }
