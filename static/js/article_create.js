69071b352d8b3df66e21fbcd5263ba3cab95cebe
예슬_노트북\tulip <tuliphan91@gmail.com>
2022-07-02 20:01:07 +0900

    refac: 이중포문으로 페이지 이미지 보여주기

--- static/js/article_create.js
+++ static/js/article_create.js
@@ -39,46 +39,15 @@ function handleArticleCreate(){
 }
 
 async function loadArticles(response_json, a_length){
-    console.log("title:"+response_json[1]['title'])
-    console.log("img_url:"+response_json[1]['image_location'])//.substring(0,-13)
-    const imgs=response_json[1]['image_location'].split('final')[0]
-    console.log(typeof imgs)
-    console.log(imgs)
-
-    // console.log(imgs.substring(str.length()-13,str.length()))
-    const carousel_title = document.getElementById("carousel-title")
-    carousel_title.innerHTML = 'title: '+response_json[1]['title']
-    console.log(imgs+'0.png')
-    document.getElementById("carouselimg1").src = imgs+'0.png'
-    document.getElementById("carouselimg2").src = imgs+'1.png'
-    document.getElementById("carouselimg3").src = imgs+'2.png'
-    document.getElementById("carouselimg4").src = imgs+'3.png'
-    document.getElementById("carouselimg5").src= response_json[1]['image_location']
-
-    //[imgs+'0.png'], [imgs+'1.png'], [imgs+'2.png'],[imgs+'3.png'],[response_json[0]['image_location']]
-
-
-    for(i;i<10;i++){
-
-
-}
-
-
-
-    
-
-
-//캐로셀 컨테이너 전체를 반복해서 그리고 싶음
-
-
-    
-
-
-    // const article_list= document.getElementById("articles")
-    // articles.forEach(element => {
-    //     const newArticle = document.createElement("li")
-    //     newArticle.setAttribute("id", article.id)
-    //     newArticle.innerText = article.titlearticle_list.append(newArticle)
+    console.log("title:"+response_json[0]['title'])
+    console.log("img_url:"+response_json[0]['image_location'])
+    for (var i=0;i<=2;i++){
+        let imgs =response_json[i]['image_location'].split('final')[0]
+        document.getElementById("carousel-title"+i).innerHTML = 'title: '+response_json[i]['title']
+        for (var j=0;j<=3;j++){
+            document.getElementById("carouselimg"+i+"_"+(j+1)).src = imgs+j+'.png'
+        }
+        document.getElementById("carouselimg"+i+"_5").src= response_json[a_length-(i+1)]['image_location']
         
-    // });
-}
+    }
+}   
---
+++ static/js/main.js
@@ -0,0 +1,34 @@
+//캐로셀 움직이는 식
+
+
+let carouselWidth = document.getElementById('carousel-container0').offsetWidth;
+console.log('width:'+ carouselWidth)
+document.getElementById('next0').addEventListener('click', () => {
+    document.getElementById('track0').style.transform = `translateX(-${carouselWidth0}px)`;
+})
+document.getElementById('prev0').addEventListener('click', () => {
+    document.getElementById('track0').style.transform = `translateX(-${0}px)`;
+})
+
+//모달 온오프
+function modalOn0(){
+    document.getElementById('modal-bg'+0).style.display="flex"
+};
+function modalOn1(){
+    document.getElementById('modal-bg'+1).style.display="flex"
+};
+function modalOn2(){
+    document.getElementById('modal-bg'+2).style.display="flex"
+};
+
+function modalOff0(){
+    document.getElementById('modal-bg'+0).style.display="none"
+};
+function modalOff1(){
+    document.getElementById('modal-bg'+1).style.display="none"
+};
+function modalOff2(){
+    document.getElementById('modal-bg'+2).style.display="none"
+};
+
+
