3da15adfb3d536c5de365b881f4bb76518770765
SG-Marco <rollingpoe@gmail.com>
2022-07-02 22:05:13 +0900

    feature - 검색 기능 생성
    검색어를 입력하지 않거나 해당하는 게시물이 없을 경우 경고 메세지.
    성공할 경우는 검색 결과 페이지로 이동.

---
+++ static/js/main.js
@@ -0,0 +1,21 @@
+
+async function search() {
+    const words_for_search = document.getElementById("words_for_search").value;
+
+    var url = new URL(backend_base_url+`/article/search/?words=${words_for_search}`);
+    console.log(words_for_search)
+    const search_results = await fetch(url)
+      .then(response => {
+        var status_code = response.status;
+        return Promise.resolve(response.json())
+               .then(data => ({ data, status_code }))
+    })
+        
+    localStorage.setItem('search_results', JSON.stringify(search_results.data));  
+
+    if (search_results.status_code == 200) {
+      window.location.replace(`${frontend_base_url}/search_result.html`);
+    } else {
+      alert(search_results.data.message)
+  }
+}
