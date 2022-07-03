37b8fda4d17bd9cf913483fcd05fe5506a085ad6
joohuun <k00005781@gmail.com>
2022-07-03 16:03:09 +0900

    충돌 해결

--- static/js/article_create.js
+++ static/js/article_create.js
@@ -1,5 +1,5 @@
 //프리뷰 이미지 만들기+프리뷰 이미지 div 보이기
-function handlePromptCreate(){
+function handlePromptCreate() {
     const prompt = document.getElementById("prompt").value
     console.log(prompt)
     startImageGenerator(prompt)
@@ -8,42 +8,41 @@ function handlePromptCreate(){
     hidden_page.classList.remove('hidden_box')
     hidden_page.classList.add('hidden_box')
     const hid_box = document.querySelector(".hid_box")
-    hid_box.style.display="flex";
+    hid_box.style.display = "flex";
     const hid_text_box = document.querySelector(".hid_text_box")
-    hid_text_box.style.display="flex";
-
-
+    hid_text_box.style.display = "flex";
 }
 
 //프리뷰 이미지 구현
-function showPromptImage(path){
+function showPromptImage(path) {
     console.log(title)
-    console.log(path,10)
+    console.log(path, 10)
     document.getElementById("title").innerHTML = title;
-    document.getElementById("main_img").src=path;
+    document.getElementById("main_img").src = path;
 }
 
 
 //아티클 생성
-function handleArticleCreate(){
+function handleArticleCreate() {
 
     const title = document.getElementById("title").innerText;
     const img_url = document.getElementById("main_img").src;
     let is_active = document.getElementById("is_active").value
-    if(is_active=="on"){
-        is_active=true
-    }else{
-        is_active=false
+    if (is_active == "on") {
+        is_active = true
+    } else {
+        is_active = false
     }
     const exposure_end_date = document.getElementById("exposure_end_date").value
     postArticle(title, img_url, is_active, exposure_end_date)
 }
+
 //댓글 생성
-function handleCommentCreate(id){
-    const comment = document.getElementById("idx-comment"+id).value || document.getElementById("modal-comment"+id).value
-    const article_id = document.getElementById("carousel-id"+id).innerText;
+function handleCommentCreate(id) {
+    const comment = document.getElementById("idx-comment" + id).value || document.getElementById("modal-comment" + id).value
+    const article_id = document.getElementById("carousel-id" + id).innerText;
     postComment(comment, article_id)
-    
+
 }
 
 
@@ -63,19 +62,13 @@ function handleCommentCreate(id){
 //     }
 // }
 
-// 함수 실행하는지 console.log
-// ui
 
 //모달 내용 불러오기
-async function loadModals(response_json){
-    for (var i=0;i<=2;i++){
-        document.getElementById('modal-img'+i).src = response_json[i]['image_location']
-        document.getElementById('modal-title'+i).innerHTML = response_json[i]['title']
-        document.getElementById("modal-author"+i).innerHTML = 'author: '+response_json[i]['user']
+async function loadModals(response_json) {
+    for (var i = 0; i <= 2; i++) {
+        document.getElementById('modal-img' + i).src = response_json[i]['image_location']
+        document.getElementById('modal-title' + i).innerHTML = response_json[i]['title']
+        document.getElementById("modal-author" + i).innerHTML = 'author: ' + response_json[i]['user']
     }
-
 }
-
-
-
 
