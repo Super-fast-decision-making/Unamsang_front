9a106671837c9d8f0f38e54ec4aee223a5f7288c
예슬_노트북\tulip <tuliphan91@gmail.com>
2022-07-03 13:48:38 +0900

    feat:코멘트 api 완성

--- static/js/main.js
+++ static/js/main.js
@@ -20,26 +20,6 @@ async function search() {
   }
 }
 
-//캐로셀 움직이는 식
-
-
-// let carouselWidth = document.getElementById('carousel-container0').offsetWidth;
-// console.log('width:'+ carouselWidth)
-// document.getElementById('next0').addEventListener('click', () => {
-//     document.getElementById('track0').style.transform = `translateX(-${carouselWidth0}px)`;
-// })
-// document.getElementById('prev0').addEventListener('click', () => {
-//     document.getElementById('track0').style.transform = `translateX(-${0}px)`;
-// })
-
-// window.addEventListener("load", function(){
-//     document.getElementById("modal-btn"+0).onclick=function(){
-//         document.getElementById('modal-bg'+0).style.display="flex"
-//     }
-
-// })
-
-
 
 
 
@@ -48,7 +28,8 @@ function modalOn(id){
     document.getElementById('modal-bg'+id).style.display="flex"
 };
 
-
 function modalOff(id){
     document.getElementById('modal-bg'+id).style.display="none"
 };
+
+
